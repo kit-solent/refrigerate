8f4f1275c38b6d52847a8cb12cc48bc6552a3833
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-07-08 09:38:02 +1200

    SaveBot commit at 2024-07-08 09:38:02

--- core/map.gd
+++ core/map.gd
@@ -1,11 +1,24 @@
 extends TileMap
 
-func merge_lots(list:Array):
-	for i in range(1,len(list)): # loop over all indexs in the list except 0 (the first one)
-		var merge=Geometry2D.merge_polygons(list[0],list[i])
-		if len(merge)==1: # if the merge was a success...
-			pass
-
+func merge_polygons(list:Array):
+	var result=[]
+	while true:
+		var merged_any=false
+		for i in range(1,len(list)): # loop over all indexs in the list except 0 (the first one)
+			var merge=Geometry2D.merge_polygons(list[0],list[i])
+			if len(merge)==1: # if the merge was a success...
+				list.remove_at(i)
+				list[0]=merge[0]
+				merged_any=true
+				break
+		if not merged_any:
+			# if none of the polygons merged then move on.
+			result.append(list.pop_front())
+		if len(list)<=1:
+			if len(list)==1:
+				result.append(list[0])
+			break
+	return result
 
 func create_regions():
 	for id in [1,2,3,4,5]:
@@ -19,23 +32,7 @@ func create_regions():
 				map_to_local(cell)+Vector2(+tile_set.tile_size.x/2,+tile_set.tile_size.y/2), # bottom right ( ⌞ )
 				map_to_local(cell)+Vector2(-tile_set.tile_size.x/2,+tile_set.tile_size.y/2), # bottom left ( ⌟ )
 			]))
-		# now we need to join the seperate polygons.
-		while true:
-			var polygons_to_remove = []
-			for child_index in len(shapes):
-				var polygon1:PackedVector2Array = shapes[child_index]
-				for child_subindex in child_index-1:
-					var polygon2:PackedVector2Array = shapes[child_subindex]
-					var merged_polygon = Geometry2D.merge_polygons(polygon1,polygon2)
-					if merged_polygon.size() != 1:
-						continue
-					polygon2 = merged_polygon[0]
-					polygons_to_remove.append(polygon1)
-					break
-			if polygons_to_remove.size() == 0:
-				break
-			for polygon_to_remove in polygons_to_remove:
-				shapes.erase(polygon_to_remove)
+		shapes=merge_polygons(shapes)
 		for i in shapes:
 			var new=Area2D.new()
 			new.add_child(CollisionPolygon2D.new())
