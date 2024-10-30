026f8ed1adce3fa0f91770045353e638d915b311
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-10-30 16:50:41 +1300

    SaveBot commit at 2024-10-30 16:50:41

--- portals/portal.gd
+++ portals/portal.gd
@@ -60,16 +60,39 @@ func set_view(target:Node):
 	#$line_2d2.points = points
 
 func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
+	"""
+	NOTE: target, cast_point, and bounds are all in local coords.
+	"""
+	
 	if not (bounds.has_point(target) and bounds.has_point(cast_point)):
 		printerr("Invalid arguments to get_cast_point, both points must be inside the given bounds.")
 		return ERR_INVALID_PARAMETER
 	
-	var angle = target.angle_to_point(cast_point)
+	# the coordinates of the corners of the viewport rectangle relative to our origin.
+	var viewport_top_left     = bounds.position
+	var viewport_top_right    = viewport_top_left  + Vector2.RIGHT * bounds.size.x
+	var viewport_bottom_left  = viewport_top_left  + Vector2.DOWN  * bounds.size.y
+	var viewport_bottom_right = viewport_top_right + Vector2.DOWN  * bounds.size.y
 	
-	if angle:pass
+	var angle_top_left     = fix_angle(target.angle_to_point(viewport_top_left))
+	var angle_top_right    = fix_angle(target.angle_to_point(viewport_top_right))
+	var angle_bottom_left  = fix_angle(target.angle_to_point(viewport_bottom_left))
+	var angle_bottom_right = fix_angle(target.angle_to_point(viewport_bottom_right))
+	
+	# this angle is always positive and goes from 0 along the line y= -x clockwise to 2pi back on that line.
+	var angle = fix_angle(target.angle_to_point(cast_point)) + 0.25 * PI
+	
+	if 0 < angle <= 1 * PI/4:
+		print("The angle is to the right including the bottom right point.")
+	elif 1 * PI/4 < angle <= 2 * PI/4:
+		print("The angle is to the right including the bottom right point.")
+	elif 2 * PI/4 < angle <= 90000:
+		print("The angle is downwards including the bottom left point.")
 
-func is_between(value:float, a:float, b:float):
+
+
+func fix_angle(angle:float):
 	"""
-	Returns true if `value` is between a and b inclusive of both a and b.
+	Converts `angle` into math conventions and forces it to be positive (regardless of the argument rule).
 	"""
-	pass
+	return angle + PI
