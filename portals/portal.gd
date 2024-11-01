a8f54a65c783362afece9c319d2e2fe7251d1c14
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-01 16:48:40 +1300

    SaveBot commit at 2024-11-01 16:48:40

--- portals/portal.gd
+++ portals/portal.gd
@@ -9,33 +9,24 @@ func _ready():
 	$sub_viewport.world_2d
 
 func _process(delta: float):
-	pass # use set_view here.
+	if Input.is_action_just_pressed("debug key"):
+		$marker_2d.global_position = get_global_mouse_position()
+		get_cast_point($marker_2d.position, get_viewport_rect().get_center(), get_viewport_rect())
 
-func set_view(target:Node):
-	var target_pos = to_local(target.global_position)
-	var viewport_rect = get_viewport_rect()
+func fix_angle(angle:float):
+	"""
+	Forces angle to be positive.
+	"""
+	if angle < 0:
+		angle += 2 * PI
 	
-	# the coordinates of the corners of the viewport rectangle relative to our origin.
-	var viewport_top_left = to_local(viewport_rect.position)
-	var viewport_top_right = viewport_top_left + Vector2.RIGHT * viewport_rect.size.x
-	var viewport_bottom_left = viewport_top_left + Vector2.DOWN * viewport_rect.size.y
-	var viewport_bottom_right = viewport_top_right + Vector2.DOWN * viewport_rect.size.y
-	
-	$line_2d2.points = [
-		viewport_top_left * 0.9,
-		viewport_top_right * 0.9,
-		viewport_bottom_right * 0.9,
-		viewport_bottom_left * 0.9
-	]
-	
-	var points = [point_a,point_b]
-
-	$view.polygon = points
-	#$line_2d2.points = points
+	return float(angle)
 
 func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 	"""
 	NOTE: target, cast_point, and bounds are all in local coords.
+	target is the position from which the line is cast.
+	cast_point is the point towards which the line is cast.
 	"""
 	
 	if not (bounds.has_point(target) and bounds.has_point(cast_point)):
@@ -48,36 +39,118 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 	var viewport_bottom_left  = viewport_top_left  + Vector2.DOWN  * bounds.size.y
 	var viewport_bottom_right = viewport_top_right + Vector2.DOWN  * bounds.size.y
 	
-	var angle_tl = target.angle_to_point(viewport_top_left)
-	var angle_tr = target.angle_to_point(viewport_top_right)
-	var angle_bl = target.angle_to_point(viewport_bottom_left)
-	var angle_br = target.angle_to_point(viewport_bottom_right)
+	# the angles from the cast_point to the corners of the screen.
+	var angle_tl = fix_angle(target.angle_to_point(viewport_top_left))
+	var angle_tr = fix_angle(target.angle_to_point(viewport_top_right))
+	var angle_bl = fix_angle(target.angle_to_point(viewport_bottom_left))
+	var angle_br = fix_angle(target.angle_to_point(viewport_bottom_right))
 	
-	var angle = target.angle_to_point(cast_point)
+	# the angles pointing directly up, down, left, and right.
+	var angle_right = 0
+	var angle_down  = 1 * PI/4
+	var angle_left  = 2 * PI/4
+	var angle_up    = 3 * PI/4
 	
-	var direction = cast_point - target
-	var xMult = 0
-	var yMult = 0
+	# the angle from the target to the cast point.
+	var angle = fix_angle(target.angle_to_point(cast_point))
 	
-	if direction.x != 0:
-		if direction.x > 0:
-			print(viewport_top_right.x - target.x)
-			print(direction.x)
-			xMult = (viewport_top_right.x - target.x)/direction.x
-		else:
-			print(viewport_top_left.x - target.x)
-			print(direction.x)
-			xMult = (viewport_top_left.x - target.x)/direction.x
+	# the edge which the line will intersect as a Vector2.
+	# Vector2.UP reperesents the top edge, Vector2.RIGHT reperesents the right edge etc.
+	var edge:Vector2
 	
-	print(xMult)
+	# the point at which the line intersects the edge of the view.
+	var intersect:Vector2
 	
-	# so that the angle is only ever positive
-	if angle < 0:
-		angle += 2*PI
+	# the distance from the target to the edge towards which it point.
+	var dist:float
+	# a variable used for internal right angle triangle calculations
+	var theta:float
+	# the distance from the target to the intersect point along the other angle to dist
+	var distance:float
+	# this reperesents the sign on distance. true is positive and false is negative.
+	var dsign:bool
 	
+	# calculate which edge the line points towards based on the angle.
+	# also calculate the point of intersection.
+	if angle_br < angle and angle < angle_bl:
+		edge = Vector2.DOWN
+		
+		# calculate the vertical distance from the target to the bottom edge.
+		dist = viewport_bottom_left.y - target.y
+		
+		if angle < angle_down:
+			# the angle is to the right of straight down.
+			theta = PI/4 - angle
+			dsign = true
+		elif angle >= angle_down:
+			# the angle is to the left of or is straight down.
+			theta = angle - PI/4
+			dsign = false
+		
+		distance = abs(dist) * tan(theta)
+		
+		intersect = Vector2((1 if dsign else -1) * distance, dist)
+		
+	elif angle_bl < angle and angle < angle_tl:
+		edge = Vector2.LEFT
+		
+		# calculate the horrisontal distance from the target to the left edge.
+		dist = viewport_top_left - target.x
+		
+		if angle < angle_left:
+			# the angle is below straight left.
+			theta = PI - angle
+			dsign = true
+		elif angle >= angle_left:
+			# the angle is above or is straight left.
+			theta = angle - PI
+			dsign = false
+		
+		distance = abs(dist) * tan(theta)
+		
+		intersect = Vector2(dist, (1 if dsign else -1) * distance)
+		
+	elif angle_tl < angle and angle < angle_tr:
+		edge = Vector2.UP
+		
+		# calculate the vertical distance from the target to the top edge.
+		dist = viewport_top_left.y - target.y
+		
+		if angle < angle_up:
+			# the angle is to the left of straight up.
+			theta = angle - PI
+			dsign = false
+		elif angle >= angle_up:
+			# the angle is to the right of or is straight up.
+			theta = 2*PI - angle
+			dsign = true
+		
+		## NOTE: interesting point some of the other theta calculations are just
+		## the negative of their pair. This is actually the case here but 2*PI - angle
+		## is equivalent and prevents negative results.
+		
+		distance = abs(dist)/tan(theta)
+		
+		intersect = Vector2((1 if dsign else -1) * distance, dist)
+		
+	elif (angle > angle_tr) or (angle < angle_br):
+		edge = Vector2.RIGHT
+		
+		# calculate the horrisontal distance from the target to the right edge.
+		dist = viewport_top_right.x - target.x
+		
+		## NOTE: This angle calculation is slightly different due to the 0 angle being in the middle of the range.
+		if angle < angle_left:
+			# the angle is below or is straight right.
+			theta = angle
+			dsign = true
+		elif angle > angle_left:
+			# the angle is above straight right.
+			theta = 2*PI - angle
+			dsign = false
+		
+		distance = abs(dist) * tan(angle)
+		
+		intersect = Vector2(dist, (1 if dsign else -1) * distance)
 	
-func to_local_rect(rect:Rect2):
-	"""
-	Converts a rect in global coordinates to local coordinates.
-	"""
-	return Rect2(to_local(rect.position), rect.size)
+	$line_2d2.points = [target, cast_point, intersect]
