7d7fb3c74916a80c98bdacac4e5c8e8456b16e6c
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-02 17:04:03 +1300

    SaveBot commit at 2024-11-02 17:04:03

--- portals/portal.gd
+++ portals/portal.gd
@@ -1,10 +1,9 @@
 class_name Portal extends Node2D
 
 @export var pair:Portal
-@export var point_a:Vector2 = 32 * Vector2.UP
-@export var point_b:Vector2 = 32 * Vector2.DOWN
 
 func _ready():
+	#print(is_between(0, TAU/8, 7*TAU/8))
 	#$line.points = [point_a, point_b]
 	#$line.show()
 	pass
@@ -12,9 +11,9 @@ func _ready():
 	#$sub_viewport.world_2d
 
 func _process(delta: float):
-	if Input.is_action_just_pressed("debug key"):
-		$marker_2d.global_position = get_global_mouse_position()
-		set_view($marker_2d)
+	$marker_2d.global_position = get_global_mouse_position()
+	set_view($marker_2d)
+	pass
 
 func fix_angle(angle:float):
 	"""
@@ -32,8 +31,53 @@ func is_between(angle:float, a:float, b:float):
 	var big_angle = max(a, b)
 	var small_angle = min(a, b)
 	var diff = big_angle - small_angle
-	if diff < TAU:pass
+	print(big_angle)
+	print(small_angle)
+	print(diff)
+	# this is the acute direction from small to big
+	var direction = diff < TAU # true is clockwise and false is anticlockwise
+	# this is the absolute angle between a and b
+	var abs_diff = diff if direction else TAU - diff
 	
+	print("direction is")
+	print(direction)
+	if direction:
+		# clockwise is just a standard check
+		return small_angle < angle and angle < big_angle
+	else:
+		# anticlockwise only happens when the positive x axis is inside the angle
+		print(angle)
+		print(small_angle)
+		print(big_angle)
+		return angle < small_angle or angle > big_angle
+
+
+# TODO: This needs finishing. Consider: https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+func get_portal_line(line:PackedVector2Array, bounds:Rect2):
+	"""
+	returns the portion of the portal line that is inside the given bounds
+	"""
+	# remove all points where both the point and the points to either side of it are outside the bounds.
+	# record all points that are outside but that have an adjacent point inside so that these can be moved
+	# to the border.
+	var points = []
+	
+	# move all out of bound points toward their previous point
+	
+	# compute the intersection points and use them in place of the out of bounds points.
+	for i in range(len(points)):
+		if bounds.has_point(points[i]):
+			points.append(points[i])
+		else:
+			var in_point = points[i - 1] if i >= 1 else points[i + 1]
+			
+			var cast_point = get_cast_point(in_point, points[i], bounds)
+			
+			points.append(cast_point[0])
+	
+	
+	
+	return points
 
 func set_view(target:Node):
 	var global_bounds = get_viewport_rect()
@@ -46,65 +90,33 @@ func set_view(target:Node):
 	
 	var target_pos = to_local(target.global_position)
 	
-	@warning_ignore("shadowed_variable")
-	var point_a = $line.points[0] + $line.position
-	@warning_ignore("shadowed_variable")
-	var point_b = $line.points[1] + $line.position
-	
-	var cast_a = get_cast_point(target_pos, point_a, bounds)
-	var cast_b = get_cast_point(target_pos, point_b, bounds)
-	
 	# the angles from the cast_point to the corners of the screen.
 	var angle_tl = fix_angle(target_pos.angle_to_point(viewport_top_left))
 	var angle_tr = fix_angle(target_pos.angle_to_point(viewport_top_right))
 	var angle_bl = fix_angle(target_pos.angle_to_point(viewport_bottom_left))
 	var angle_br = fix_angle(target_pos.angle_to_point(viewport_bottom_right))
 	
-	var angle_a = fix_angle(target_pos.angle_to_point(point_a))
-	var angle_b = fix_angle(target_pos.angle_to_point(point_b))
+	# get the points that make up the portal border.
+	var portal_points = $line.points
 	
-	# find and record the larger and smaller of the angles and points.
-	var angle1 # smaller
-	var cast1
-	var point1
-	var angle2 # larger
-	var cast2
-	var point2
-	if angle_a < angle_b:
-		angle1 = angle_a
-		cast1  = cast_a
-		point1 = point_a
-		
-		angle2 = angle_b
-		cast2  = cast_b
-		point2 = point_b
-	else:
-		angle1 = angle_b
-		cast1  = cast_b
-		point1 = point_b
-		
-		angle2 = angle_a
-		cast2  = cast_a
-		point2 = point_a
-		
-	var points = [point1, cast1]
+	var cast_points = []
+	var edges = []
+	var angles = []
+	for i in portal_points:
+		var err = get_cast_point(target_pos, i, bounds)
+		if err:
+			return ERR_HELP
+		cast_points.append(err[0])
+		edges.append(err[1])
+		angles.append(fix_angle(target_pos.angle_to_point(i)))
 	
-	var angles = {
-		angle_br:viewport_bottom_right,
-		angle_bl:viewport_bottom_left,
-		angle_tl:viewport_top_left,
-		angle_tr:viewport_top_right
-	}
-	
-	# this is the angles from smallest to largest
-	for a in angles:
-		if angle1 < a and a < angle2:
-			# if i is between angle1 and angle2
-			points.append(angles[a])
-			
-	points.append_array([cast2, point2])
+	# start the polygon with all the points that make up the portal line.
+	var points = portal_points
+	cast_points.reverse()
+	points.append_array(cast_points)
 	
 	$view.polygon = points
+	$line2.points = points
 
 func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 	"""
@@ -112,8 +124,8 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 	target is the position from which the line is cast.
 	cast_point is the point towards which the line is cast.
 	"""
-	if not (bounds.has_point(target) and bounds.has_point(cast_point)):
-		printerr("Invalid arguments to get_cast_point, both points must be inside the given bounds.")
+	if not bounds.has_point(target):
+		printerr("Invalid arguments to get_cast_point, `target` must be inside the given bounds.")
 		return ERR_INVALID_PARAMETER
 	
 	# the coordinates of the corners of the viewport rectangle relative to our origin.
@@ -155,8 +167,8 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 	
 	# calculate which edge the line points towards based on the angle.
 	# also calculate the point of intersection.
-	if angle_br < angle and angle < angle_bl:
-		edge = Vector2.DOWN
+	if angle_br < angle and angle <= angle_bl:
+		edge = Vector2.DOWN # including bottom left 
 		
 		# calculate the vertical distance from the target to the bottom edge.
 		dist = viewport_bottom_left.y - target.y
@@ -173,8 +185,8 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 		
 		intersect = Vector2((1 if dsign else -1) * distance, dist)
 		
-	elif angle_bl < angle and angle < angle_tl:
-		edge = Vector2.LEFT
+	elif angle_bl < angle and angle <= angle_tl:
+		edge = Vector2.LEFT # including top left
 		
 		# calculate the horrisontal distance from the target to the left edge.
 		dist = viewport_top_left.x - target.x
@@ -192,8 +204,8 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 		
 		intersect = Vector2(dist, (1 if dsign else -1) * distance)
 		
-	elif angle_tl < angle and angle < angle_tr:
-		edge = Vector2.UP
+	elif angle_tl < angle and angle <= angle_tr:
+		edge = Vector2.UP # including top right
 		
 		# calculate the vertical distance from the target to the top edge.
 		dist = viewport_top_left.y - target.y
@@ -207,7 +219,7 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 			theta = TAU - angle
 			dsign = true
 		
-		## NOTE: interesting point some of the other theta calculations are just
+		## NOTE: interesting point, some of the other theta calculations are just
 		## the negative of their pair. This is actually the case here but TAU - angle
 		## is equivalent and prevents negative results.
 		
@@ -215,8 +227,8 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 		
 		intersect = Vector2((1 if dsign else -1) * distance, dist)
 		
-	elif (angle > angle_tr) or (angle < angle_br):
-		edge = Vector2.RIGHT
+	elif angle > angle_tr or angle <= angle_br:
+		edge = Vector2.RIGHT # including bottom right
 		
 		# calculate the horrisontal distance from the target to the right edge.
 		dist = viewport_top_right.x - target.x
@@ -230,12 +242,12 @@ func get_cast_point(target:Vector2, cast_point:Vector2, bounds:Rect2):
 			# the angle is above straight right.
 			theta = TAU - angle
 			dsign = false
-
+		
 		distance = abs(dist) * tan(theta)
-
+		
 		intersect = Vector2(dist, (1 if dsign else -1) * distance)
 	
 	# currently the intersect is relative to the target point so convert to local coords by adding the target point.
 	intersect += target
 	
-	return intersect
+	return [intersect, edge]
