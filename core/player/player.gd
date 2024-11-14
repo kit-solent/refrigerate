ae0c3cfbcdb347298fdfc0a041a974424788d0b2
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-15 09:24:42 +1300

    SaveBot commit at 2024-11-15 09:24:42

--- core/player/player.gd
+++ core/player/player.gd
@@ -7,35 +7,36 @@ var gravity_strength=ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var acceleration:int = 10
 @export var deacceleration:int = 40
 @export var terminal_velocity:int = 1500
+#enum modes {TopDown, PlatformerDown, PlatformerUp, PlatformerLeft, PlatformerRight}
 
 func _integrate_forces(_state:PhysicsDirectBodyState2D):
-	if mode==Core.modes.TopDown: # TopDown mode.
+	if mode == Core.modes.TopDown: # TopDown mode.
 		pass
 	else: # one of the Platformer modes.
 		linear_velocity += Core.gravity[mode]*gravity_strength*_state.step
 		var dir = Input.get_axis("player left","player right")
 		if dir:
-			if mode == 1:
+			if mode == Core.modes.PlatformerDown:
 				linear_velocity.x = lerp(linear_velocity.x,dir*move_velocity,acceleration*_state.step)
-			elif mode == 2:
+			elif mode == Core.modes.PlatformerUp:
 				linear_velocity.x = lerp(linear_velocity.x,-dir*move_velocity,acceleration*_state.step)
-			elif mode == 3:
+			elif mode == Core.modes.PlatformerLeft:
 				linear_velocity.y = lerp(linear_velocity.y,dir*move_velocity,acceleration*_state.step)
-			elif mode == 4:
+			elif mode == Core.modes.PlatformerRight:
 				linear_velocity.y = lerp(linear_velocity.y,-dir*move_velocity,acceleration*_state.step)
 		else:
-			if mode in [1,2]: # If the gravity direction is down or up.
+			if mode in [Core.modes.PlatformerDown,Core.modes.PlatformerUp]: # If the gravity direction is down or up.
 				linear_velocity.x = lerp(linear_velocity.x,0.0,deacceleration*_state.step) 
-			elif mode in [3,4]: # If the gravity direction is left or right.
+			elif mode in [Core.modes.PlatformerLeft,Core.modes.PlatformerRight]: # If the gravity direction is left or right.
 				linear_velocity.y = lerp(linear_velocity.y,0.0,deacceleration*_state.step)
 		if Input.is_action_just_pressed("player jump"):
-			if mode == 1: # Gravity down
+			if mode == Core.modes.PlatformerDown: # Gravity down
 				linear_velocity.y += -jump_velocity
-			elif mode == 2: # Gravity up
+			elif mode == Core.modes.PlatformerUp: # Gravity up
 				linear_velocity.y += jump_velocity
-			elif mode == 3: # Gravity left
+			elif mode == Core.modes.PlatformerLeft: # Gravity left
 				linear_velocity.x += jump_velocity
-			elif mode == 4: # Gravity right
+			elif mode == Core.modes.PlatformerRight: # Gravity right
 				linear_velocity.x += -jump_velocity
 		linear_velocity.y=clamp(linear_velocity.y,-terminal_velocity,terminal_velocity)
 		linear_velocity.x=clamp(linear_velocity.x,-terminal_velocity,terminal_velocity)
@@ -44,11 +45,11 @@ func _on_body_entered(body):
 	if body.is_in_group("terrain"):pass
 
 func set_mode(_mode:int):
-	mode=_mode
-	rotation=Core.gravity[_mode].angle()-(PI/2)
-	if _mode==0:
-		$topdown_collision.set_deferred("disabled",false)
-		$platformer_collision.set_deferred("disabled",true)
+	mode = _mode
+	rotation = Core.gravity[_mode].angle()-(TAU/4)
+	if _mode == Core.modes.TopDown:
+		$topdown_collision.set_deferred("disabled", false)
+		$platformer_collision.set_deferred("disabled", true)
 	else:
-		$topdown_collision.set_deferred("disabled",true)
-		$platformer_collision.set_deferred("disabled",false)
+		$topdown_collision.set_deferred("disabled", true)
+		$platformer_collision.set_deferred("disabled", false)
