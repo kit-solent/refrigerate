7ba1916a29c9529b659078f1b88d00413780d1d4
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-17 20:03:22 +1300

    SaveBot commit at 2024-11-17 20:03:22

--- core/ingame.gd
+++ core/ingame.gd
@@ -1,12 +1,12 @@
 extends Control
-@onready var camera_target=$h_box_container/panel/view/view/world/players/player
+@onready var camera_target=$view/world/players/player
 
 func _ready():
-	$h_box_container/panel/view/view/world/meta/darkener.visible = not Core.debug
+	$view/world/meta/darkener.visible = not Core.debug
 	#Core.local_player=$h_box_container/panel/view/view/world/players/player
 
 func _process(_delta):
-	$h_box_container/panel/view/view/world/meta/camera.global_transform=camera_target.global_transform
+	$view/world/meta/camera.global_transform=camera_target.global_transform
 
 func get_player():
-	return $h_box_container/panel/view/view/world/players/player
+	return $view/world/players/player
