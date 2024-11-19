b7c40b91ccc9d90e27ccf281c7ef0ead791fd061
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-20 10:57:46 +1300

    SaveBot commit at 2024-11-20 10:57:46

--- core/map.gd
+++ core/map.gd
@@ -1,44 +1,42 @@
 extends TileMapLayer
-#
-#func merge_polygons(list:Array):
-	#var result=[]
-	#while true:
-		#var merged_any=false
-		#for i in range(1,len(list)): # loop over all indexs in the list except 0 (the first one)
-			#var merge=Geometry2D.merge_polygons(list[0],list[i])
-			#if len(merge)==1: # if the merge was a success...
-				#list.remove_at(i)
-				#list[0]=merge[0]
-				#merged_any=true
-				#break
-		#if not merged_any:
-			## if none of the polygons merged then move on.
-			#result.append(list.pop_front())
-		#if len(list)<=1:
-			#if len(list)==1:
-				#result.append(list[0])
-			#break
-	#return result
-#
-#func create_regions():
-	#for id in [1,2,3,4,5]:
-		#var shapes:Array
-		#for cell in get_used_cells_by_id(0,id):
-			## Construct a polygon (square) clockwise from the four corners of the cell.
-			#shapes.append(PackedVector2Array([
-				## map_to_local returns the cell centre.
-				#map_to_local(cell)+Vector2(-tile_set.tile_size.x/2,-tile_set.tile_size.y/2), # top left. ( ⌜ )
-				#map_to_local(cell)+Vector2(+tile_set.tile_size.x/2,-tile_set.tile_size.y/2), # top right ( ⌝ )
-				#map_to_local(cell)+Vector2(+tile_set.tile_size.x/2,+tile_set.tile_size.y/2), # bottom right ( ⌞ )
-				#map_to_local(cell)+Vector2(-tile_set.tile_size.x/2,+tile_set.tile_size.y/2), # bottom left ( ⌟ )
-			#]))
-		#shapes=merge_polygons(shapes)
-		#for i in shapes:
-			#var new=Area2D.new()
-			#new.add_child(CollisionPolygon2D.new())
-			#new.get_child(0).polygon=Geometry2D.offset_polygon(i,tile_set.tile_size.x/2)
-			#add_child(new)
-#
-#func _ready():
-	##create_regions()
-	#pass
+
+@onready var mode_overides = get_parent().get_node("meta/mode_overides")
+
+func create_regions(id:int):
+	"""
+	Returns an array of polygons of the merged tiles of the given id.
+	"""
+	var shapes:Array[PackedVector2Array] = []
+	for cell in get_used_cells_by_id(id):
+		# Construct a polygon (square) clockwise from the four corners of the cell.
+		shapes.append(PackedVector2Array([
+			# map_to_local returns the cell centre.
+			map_to_local(cell)+Vector2(-tile_set.tile_size.x/2.0,-tile_set.tile_size.y/2.0), # top left. ( ⌜ )
+			map_to_local(cell)+Vector2(+tile_set.tile_size.x/2.0,-tile_set.tile_size.y/2.0), # top right ( ⌝ )
+			map_to_local(cell)+Vector2(+tile_set.tile_size.x/2.0,+tile_set.tile_size.y/2.0), # bottom right ( ⌞ )
+			map_to_local(cell)+Vector2(-tile_set.tile_size.x/2.0,+tile_set.tile_size.y/2.0), # bottom left ( ⌟ )
+		]))
+	
+	# merge the polygons
+	shapes = Core.tools.merge_polygons(shapes)
+	
+	return shapes
+
+func clear_regions():
+	for region in mode_overides.get_children():
+		mode_overides.remove_child(region)
+		region.queue_free()
+
+func update_regions():
+	clear_regions()
+	
+	for id in [1, 2, 3, 4, 5]: # these are the gravity regions.
+		var regions = create_regions(id)
+		for region in regions:
+			var overide = Node.new()
+			overide.add_to_group(tile_set.get_source(id).resource_name)
+			mode_overides.add_child(overide)
+	
+
+func _ready():
+	update_regions()
