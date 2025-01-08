8a68a206af6949e08ada3c1504d7f5bd4cf232a6
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2025-01-08 15:37:05 +1300

    SaveBot commit at 2025-01-08 15:37:05

--- core/player/player.gd
+++ core/player/player.gd
@@ -1,13 +1,13 @@
 extends RigidBody2D
 
 @export_enum("TopDown","PlatformerDown","PlatformerUp","PlatformerLeft","PlaformerRight") var mode=1
-@export var move_velocity:float=1000
-@export var jump_velocity:float=500
+@export var move_velocity:float = 1000
+@export var jump_velocity:float = 50
 @export var acceleration:float = 10
 @export var deacceleration:float = 40
 @export var terminal_velocity:float = 1500
 
-func _integrate_forces(_state:PhysicsDirectBodyState2D):
+func _integrate_forces(state:PhysicsDirectBodyState2D):
 	get_gravity()
 	
 	
@@ -15,27 +15,27 @@ func _integrate_forces(_state:PhysicsDirectBodyState2D):
 		pass
 	else: # one of the Platformer modes.
 		# apply gravity.
-		linear_velocity += get_gravity()*_state.step
+		linear_velocity += get_gravity()*state.step
 		
 		var dir = Input.get_axis("player left","player right")
 		if dir:
 			if mode == Core.modes.PlatformerDown:
-				linear_velocity.x = lerp(linear_velocity.x,dir*move_velocity,acceleration*_state.step)
+				linear_velocity.x = lerp(linear_velocity.x,dir*move_velocity,acceleration*state.step)
 			elif mode == Core.modes.PlatformerUp:
-				linear_velocity.x = lerp(linear_velocity.x,-dir*move_velocity,acceleration*_state.step)
+				linear_velocity.x = lerp(linear_velocity.x,-dir*move_velocity,acceleration*state.step)
 			elif mode == Core.modes.PlatformerLeft:
-				linear_velocity.y = lerp(linear_velocity.y,dir*move_velocity,acceleration*_state.step)
+				linear_velocity.y = lerp(linear_velocity.y,dir*move_velocity,acceleration*state.step)
 			elif mode == Core.modes.PlatformerRight:
-				linear_velocity.y = lerp(linear_velocity.y,-dir*move_velocity,acceleration*_state.step)
+				linear_velocity.y = lerp(linear_velocity.y,-dir*move_velocity,acceleration*state.step)
 		else:
 			if mode in [Core.modes.PlatformerDown,Core.modes.PlatformerUp]: # If the gravity direction is down or up.
-				linear_velocity.x = lerp(linear_velocity.x,0.0,deacceleration*_state.step) 
+				linear_velocity.x = lerp(linear_velocity.x,0.0,deacceleration*state.step) 
 			elif mode in [Core.modes.PlatformerLeft,Core.modes.PlatformerRight]: # If the gravity direction is left or right.
-				linear_velocity.y = lerp(linear_velocity.y,0.0,deacceleration*_state.step)
+				linear_velocity.y = lerp(linear_velocity.y,0.0,deacceleration*state.step)
 		
 		# apply jump forces.
 		if Input.is_action_just_pressed("player jump"):
-			linear_velocity += -get_gravity()*jump_velocity
+			linear_velocity += -get_gravity()*jump_velocity*state.step
 		
 		# apply terminal velocity.
 		linear_velocity = linear_velocity.clamp(-Vector2.ONE * terminal_velocity, Vector2.ONE * terminal_velocity)
