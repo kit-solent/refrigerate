3b497d1063de74fa6d04334872a8382320af940c
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2025-01-29 16:32:52 +1300

    SaveBot commit at 2025-01-29 16:32:52

--- core/auth/start.gd
+++ core/auth/start.gd
@@ -11,3 +11,11 @@ func _on_gameplay_video_finished():
 	# the video will automatically restart but
 	# here we could play something else or stop it.
 	pass
+
+
+func _on_sign_in_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_sign_up_button_down() -> void:
+	pass # Replace with function body.
