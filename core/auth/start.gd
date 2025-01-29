2d032d6a8c9341cbab100150882d17e53aff2206
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2025-01-29 16:42:53 +1300

    SaveBot commit at 2025-01-29 16:42:53

--- core/auth/start.gd
+++ core/auth/start.gd
@@ -13,9 +13,18 @@ func _on_gameplay_video_finished():
 	pass
 
 
+@onready var tab_container = $main/main/center_container/tab_container
 func _on_sign_in_button_down() -> void:
-	pass # Replace with function body.
-
+	tab_container.current_tab = 4
 
 func _on_sign_up_button_down() -> void:
-	pass # Replace with function body.
+	tab_container.current_tab = 5
+
+func _on_source_button_down() -> void:
+	tab_container.current_tab = 1
+
+func _on_lore_button_down() -> void:
+	tab_container.current_tab = 2
+
+func _on_credits_button_down() -> void:
+	tab_container.current_tab = 3
