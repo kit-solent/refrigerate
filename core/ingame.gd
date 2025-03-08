15b10d2f5dd0ff63c6651bb10da7852abceaf153
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2025-03-08 14:50:03 +1300

    SaveBot commit at 2025-03-08 14:50:03

--- core/ingame.gd
+++ core/ingame.gd
@@ -1,20 +1,14 @@
 extends Control
 
-@export var main_shader:Shader
-@export var overlay_shader:Shader
-
 @onready var camera_target=get_player()
 
 func _ready():
-	$h_box_container/panel/view/view/world/meta/darkener.visible = not Core.debug
-	$h_box_container/panel/view.material.shader = main_shader
-	$h_box_container/panel/overlay.material.shader = overlay_shader
+	%world/meta/darkener.visible = not Core.debug
 	
 	#Core.local_player=$h_box_container/panel/view/view/world/players/player
 
 func _process(_delta):
-	$h_box_container/panel/view/view/world/meta/camera.global_transform=camera_target.global_transform
-	#$h_box_container/panel/view/view/world/meta/camera.global_position=camera_target.global_position
+	%world/meta/camera.global_transform=camera_target.global_transform
 
 func get_player():
-	return $h_box_container/panel/view/view/world/players/player
+	return %world/players/player
