6cc82d04aea0974165204c45003a9bbb23c20e1a
Max Lang <nelsonmaxlang@gmail.com>
2025-11-22 12:34:03 +1300

    SaveBot commit at 2025-11-22 12:34:03

--- features/portals/portal.gd
+++ features/portals/portal.gd
@@ -13,40 +13,32 @@ func _ready():
 	# find the bounding rectangle of the line, expand it for error margin, and assign it to the on screen notifier.
 	$on_screen_notifier.rect = Core.tools.line_bounds($line.points).grow(64)
 
+var delay_a_frame = true
+
 func _process(_delta:float):
+	# putting this in ready doesn't work for some reason so do it on the
+	# first frame that _process runs.
+	if delay_a_frame:
+		$sub_viewport.size = get_local_bounds().size
+		delay_a_frame = false
+	
 	# every frame we delete all the polygons (if any).
 	# this prevents polygons from lingering after the portal
 	# has left the screen. These can be picked up by other
 	# portals which looks bad.
 	if polygons_in_view:
 		clear_view()
-		
+	
 	# the portal is only drawn if on the local screen. This works with multiplayer, only showing the portal to those who can see it.
 	# the portal should also only be drawn if it's target is within a certain distance of the portal.
 	if pair:
 		# Move our camera to the pair portal. Global coordinates must be used because the
 		# camera is inside a viewport and so doesn't have coordinates local to the pair (I think).
-		$sub_viewport/camera_2d.global_position = pair.global_position + Vector2(256.3478, 254.47421)
+		$sub_viewport/camera_2d.global_position = pair.global_position
 		
 		# update the view
 		if $on_screen_notifier.is_on_screen():
 			set_view(target)
-	
-	# TODO: This hack gives the below positions.
-	# adding the difference to the camera position
-	# gives the right position but the "magic vector"
-	# is only as accurate as the visual mouse calibration.
-	
-	# DEBUG
-	#if Input.is_action_just_pressed("debug key"):
-	#	print("Portal: "+name)
-	#	print("  Global Position: "+str(global_position))
-	#	print("  Mouse  Position: "+str(get_global_mouse_position()))
-	
-	# portal                 Vector2(246.0, -625.0)
-	# mouse/target portal    Vector2(502.3478, -370.5258)
-	# mouse - portal = vector from us to target
-	# = Vector2(256.3478, 254.47421)
 
 # keeps trask of if there are currently polygons displayed in the view
 var polygons_in_view:bool = false
@@ -60,12 +52,18 @@ func set_view(target:Node):
 	var polygons = Core.tools.cast_polygons(to_local(target.global_position), $line.points, get_local_bounds())
 	
 	for i in polygons:
-		var new = $texture_storage.duplicate() # Polygon2D.new()
+		# duplicate the texture storage node with all
+		# its properties then replace the polygon.
+		var new = $texture_storage.duplicate()
 		new.polygon = i
 		new.show()
 		
-		# Copy the viewport texture over from the storage node to the new polygon.
-		#new.texture = $texture_storage.texture
+		# Shift the texture origin from the centre of the portal
+		# to the top left of the viewport rect (plus margin).
+		# This puts the whole texture on screen.
+		# We can't use get_local_bounds().position because that
+		# is affected by the camera smoothing.
+		new.texture_offset = -$texture_storage.texture.get_size()/2
 		
 		# add a border for debugging
 		var border = Line2D.new()
@@ -80,7 +78,6 @@ func set_view(target:Node):
 	# record the fact that there are polygons in the view that need to
 	# be cleared if this portal leaves the screen.
 	polygons_in_view = true
-
 
 func clear_view():
 	"""
